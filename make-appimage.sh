3d67155ff5e6a8cb79c1fa327eb2a2c56abb8157
Link4Electronics <link4electronics@gmail.com>
2026-02-01 21:57:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,6 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/roller.ico
 export DESKTOP=DUMMY
-export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/roller
