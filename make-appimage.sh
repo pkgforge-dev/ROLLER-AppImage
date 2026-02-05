958fa0ce9cad00cf88e03b05b0383a4ab79aa878
Link4Electronics <link4electronics@gmail.com>
2026-02-05 08:40:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,12 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q roller-git | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q roller-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/roller.ico
-export DESKTOP=DUMMY
 
 # Deploy dependencies
 quick-sharun /usr/bin/roller
