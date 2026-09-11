ce7d86ed7b45823ddf8a4c753e1a729583a980e0
Link4Electronics <link4electronics@gmail.com>
2026-09-10 22:51:32 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,9 +28,7 @@ git clone --depth 1 "$REPO" ./ROLLER
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
-cd ./ROLLER
-mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j$(nproc)
-mv -v roller ../../AppDir/bin
-cp -rv ../midi ../../AppDir/bin
+cmake -S ./ROLLER -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j$(nproc)
+mv -v build/roller ./AppDir/bin
+cp -rv ./ROLLER/midi ./AppDir/bin
